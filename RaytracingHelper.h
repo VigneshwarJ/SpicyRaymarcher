fabe2716fb3c462b4a51fb200fb8fec1f152db1c
Miranda <msa4295@rit.edu>
2022-04-28 23:54:07 -0400

    made files

---
+++ RaytracingHelper.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "RaytracingHelper.h"
+#include "DX12Helper.h"
+#include "BufferStructs.h"
+
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+class RaytracingHelper
+{
+};
+
